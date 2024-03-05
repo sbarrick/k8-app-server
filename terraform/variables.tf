ca956fb6dc827d0df53ab0c4521bca747cdab1ec
Shawn Barrick <46822875+sbarrick@users.noreply.github.com>
2024-03-05 16:09:14 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "cluster_name" {
   default = "prod_apps"
 }
+
+variables "owner" {
+  default = "devops"
+}
+
+variables "env" {
+  default = "prod"
+}
+
+variables "creator" {
+  default = "sbarrick"
+}
