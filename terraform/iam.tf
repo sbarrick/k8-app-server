19797fa1a759853a47e327e83068e48cf52e5fce
Shawn Barrick <46822875+sbarrick@users.noreply.github.com>
2024-03-05 16:15:14 -0500

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,23 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
 
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_role" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
